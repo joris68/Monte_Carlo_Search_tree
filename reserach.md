b6f5a4c443d0ce20b7737f06acb2e27dd94b84e5
joris68 <joris.gabrisch@web.de>
2024-10-23 20:08:12 +0200

    starting working now

---
+++ reserach.md
@@ -0,0 +1,9 @@
+Github repository:
+
+https://github.com/wlongxiang/mcts/blob/main/monte_carlo_tree_search.py
+
+
+Wikipedia Entry:
+
+https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
+
--- src/board.ts
+++ src/board.ts
@@ -1,5 +1,8 @@
-
-
-type GameState = {
-     
+// represents the board game
+type board = {
+     checkers : number [];
+     blackCheckers : number [];
+     whiteChckers : number [];
+     whiteCaught : number;
+     blackCaught : number;
 }
