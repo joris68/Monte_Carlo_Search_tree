b82282edb38423fb5e58c4f5cbcca9f47fe36163
joris68 <joris.gabrisch@web.de>
2024-11-02 22:39:16 +0100

    changed roll dice function

--- src/algo.ts
+++ src/algo.ts
@@ -2,5 +2,9 @@
 function rollDice(): number [] {
      const roll1 = Math.floor(Math.random() * 6) + 1;
      const roll2 = Math.floor(Math.random() * 6) + 1;
+     if(roll1 === roll2){
+          return [roll1, roll1, roll1, roll1];
+     }
      return [roll1, roll2];
 }
+
